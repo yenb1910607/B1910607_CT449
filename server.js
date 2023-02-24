6f096340d1b3e800edeab13dc2efa5f912fea941
yenb1910607 <yenb1910607@student.ctu.edu.vn>
2023-02-24 22:38:26 +0700

    Cai dat cac handler truy xuat CSDL

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const app = require("./app");
 const config = require("./app/config");
+const MongoDB = require("./app/utils/mongodb.util");
 
 
-const PORT = config.app.port;
-app.listen(PORT, () => {
+async function startServer() {
+    try {
+        await MongoDB.connect(config.db.uri);
+        console.log("Connected to the database!");
 
-    console.log('Server is running on port ${PORT}.');
-});
+        const PORT = config.app.port;
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.log("Cannot connect to the database!", error);
+        process.exit();
+    }
+}
+
+startServer();
